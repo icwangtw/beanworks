0a7180d17d78815cbe63701f0abcdc13bea6db2c
icwangtw <stephenicwang@gmail.com>
2018-07-26 12:31:14 -0700

    prints result to .txt

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const XeroClient = require('xero-node').AccountingAPIClient;
 const config = require('./config.json');
+const fs = require('fs');
 
 (async () => {
-
-    // You can initialise Private apps directly from your configuration
-    let xero = new XeroClient(config);
-
-    const accountInfo = await xero.accounts.get();
-
-    const vendorInfo = await xero.contacts.get();
-
-    console.log('All account info:', accountInfo);
-
-    console.log('All vendor info:', vendorInfo)
-
+  let xero = new XeroClient(config);
+  const accountInfo = await xero.accounts.get();
+  const vendorInfo = await xero.contacts.get();
+  fs.writeFile('Account.txt', JSON.stringify(accountInfo.Accounts), function (err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log('finish writing account info');
+  });
+  fs.writeFile('Vendor.txt', JSON.stringify(vendorInfo.Contacts), function (err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log('Finish writing vendor info');
+  });
 })();
