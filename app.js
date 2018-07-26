0dc0a4a26e1ec9545f9437445b4946298796ebdf
icwangtw <stephenicwang@gmail.com>
2018-07-26 15:23:17 -0700

    added error checking'

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
 const XeroClient = require('xero-node').AccountingAPIClient;
 const config = require('./config.json');
 const fs = require('fs');
-
+const xero = new XeroClient(config);
 (async () => {
-  let xero = new XeroClient(config);
-  const accountInfo = await xero.accounts.get();
-  const vendorInfo = await xero.contacts.get();
-  fs.writeFile('Account.txt', JSON.stringify(accountInfo.Accounts), function (err) {
-    if (err) {
-      return console.log(err);
-    }
-    console.log('finish writing account info');
+  try {
+    const accountInfo = await xero.accounts.get();
+    fs.writeFile('Account.txt', JSON.stringify(accountInfo.Accounts), function (err) {
+      if (err) {
+        return console.log('File writing error: ', err);
+      }
+    console.log('Finish writing account info');
   });
-  fs.writeFile('Vendor.txt', JSON.stringify(vendorInfo.Contacts), function (err) {
-    if (err) {
-      return console.log(err);
-    }
+  } catch(err) {
+    console.log('Xero access error:', err)
+  }
+
+  try {
+    const vendorInfo = await xero.contacts.get();
+    fs.writeFile('Vendor.txt', JSON.stringify(vendorInfo.Contacts), function (err) {
+      if (err) {
+        return console.log('File writing error: ', err);
+      }
     console.log('Finish writing vendor info');
   });
+  } catch(err) {
+    console.log('Xero access error:', err)
+  }
+
 })();
