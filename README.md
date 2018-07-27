adb1d3c443713285e7710c723a956a6cf3287c42
icwangtw <stephenicwang@gmail.com>
2018-07-27 11:55:41 -0700

    moved location of files

---
+++ README.md
@@ -0,0 +1,11 @@
+# Beanworks Coding Exercise
+Written using Nodejs
+
+
+## Getting Started
+
+3. Run
+`npm start`
+The ouput would be stored as `Account.json` and `Vendor.json` in  `\results`
+
+## Dependencies
--- app.js
+++ app.js
@@ -1,30 +1,31 @@
 const XeroClient = require('xero-node').AccountingAPIClient;
-const config = require('./config.json');
+const config = require('./keys/config.json');
 const fs = require('fs');
 const xero = new XeroClient(config);
+
 (async () => {
   try {
     const accountInfo = await xero.accounts.get();
-    fs.writeFile('Account.txt', JSON.stringify(accountInfo.Accounts), function (err) {
+    fs.writeFile('results/Account.json', JSON.stringify(accountInfo.Accounts), function (err) {
       if (err) {
         return console.log('File writing error: ', err);
       }
     console.log('Finish writing account info');
   });
   } catch(err) {
-    console.log('Xero access error:', err)
+    console.log('Xero access error:', err);
   }
 
   try {
     const vendorInfo = await xero.contacts.get();
-    fs.writeFile('Vendor.txt', JSON.stringify(vendorInfo.Contacts), function (err) {
+    fs.writeFile('results/Vendor.json', JSON.stringify(vendorInfo.Contacts), function (err) {
       if (err) {
         return console.log('File writing error: ', err);
       }
     console.log('Finish writing vendor info');
   });
   } catch(err) {
-    console.log('Xero access error:', err)
+    console.log('Xero access error:', err);
   }
 
 })();
